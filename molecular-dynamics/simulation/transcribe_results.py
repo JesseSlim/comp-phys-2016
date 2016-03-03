6f4ce750113ac8d1b22c3bdb1924342c10c2d801
Jesse Slim <jesse.g.slim@gmail.com>
2016-03-03 10:37:32 +0100

    corrected T and Cv calculated values

--- molecular-dynamics/simulation/transcribe_results.py
+++ molecular-dynamics/simulation/transcribe_results.py
@@ -4,7 +4,7 @@ Ms = [6]
 Ts = np.linspace(0.0, 4.0, 17)
 rhos = np.concatenate((np.array([0.02, 0.05]), np.linspace(0.1, 2.0, 20)))
 
-simJobs = [5]
+simJobs = np.arange(1,6)
 
 resultFiles = []
 
@@ -21,4 +21,8 @@ for rFile in resultFiles:
 		for key in data:
 			if not key == "distance_histogram":
 				newData[key] = data[key]
+		# calculation of instantaneous temperature contained an error, recalculate (and recalculate cv)
+		newData["temperature"] = 2*data["kinetic_energy"]/(3*(data["n"] - 1))
+		
+		newData["cv"] = 1 + 1/(3 * data["n"] * np.mean(newData["temperature"])) * np.mean(data["virial"])
 		np.savez("results-nohist/" + rFile, **newData)
