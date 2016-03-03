47ab16d1da80c2a16eb139fcc847ed7856e6e16a
Jesse Slim <jesse.g.slim@gmail.com>
2016-03-03 22:56:31 +0100

    simulatiebatch voor lange simulaties

---
+++ molecular-dynamics/simulation/run_simulation_long.py
@@ -0,0 +1,34 @@
+from ArgonSimulation import ArgonSimulation
+import numpy as np
+import sys
+import time 
+
+sim = ArgonSimulation()
+sim.rescalingPeriod = 40
+sim.rescalingTime = 40.0
+sim.equilibrationTime = 50.0
+sim.simulationTime = 110.0
+
+Ms = [6]
+Ts = np.linspace(0.25, 4.0, 16)
+rhos = np.concatenate((np.array([0.02, 0.05]), np.linspace(0.1, 2.0, 20)))
+ 
+simJob = 1
+
+rhoblock_start = int(sys.argv[1])
+rhoblock_end = int(sys.argv[2])
+rhos = rhos[rhoblock_start:rhoblock_end]
+
+print("rhos for this simulation batch: " + str(rhos))
+
+for M in Ms:
+    for T in Ts:
+        for rho in rhos:
+            print("Starting simulation with parameters (sim nr " + str(simJob) + "): ")
+            print("M:   " + str(M))
+            print("T:   " + str(T))
+            print("rho: " + str(rho))
+            filename = "results-longrun/argon-M-%d-T-%.2f-rho-%.2f-simjob-%d.npz" % (M,T,rho,simJob)
+            sim.initialise_arrays(T, rho, M)
+            sim.do_simulation()
+            sim.save_results(filename)
