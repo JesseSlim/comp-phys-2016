4c6cb7015bb332db4d532ae8c9236371e89bed64
Jesse Slim <jesse.g.slim@gmail.com>
2016-03-05 18:32:54 +0100

    Dataverwerking van de korte en lange runs (in de korte run zitten een paar bugs, lange is oke)

--- molecular-dynamics/simulation/run_simulation.py
+++ molecular-dynamics/simulation/run_simulation.py
@@ -4,19 +4,22 @@ import sys
 import time 
 
 sim = ArgonSimulation()
+sim.rescalingPeriod = 50
+sim.rescalingTime = 30.0
+sim.equilibrationTime = 40.0
+sim.simulationTime = 60.0
 
 Ms = [6]
-Ts = np.linspace(0.0, 4.0, 17)
+Ts = [0.5]
 rhos = np.concatenate((np.array([0.02, 0.05]), np.linspace(0.1, 2.0, 20)))
-
+ 
 simJob = 5
 
-Tsblock_start = int(sys.argv[1])
-Tsblock_end = int(sys.argv[2])
+rhoblock_start = int(sys.argv[1])
+rhoblock_end = int(sys.argv[2])
+rhos = rhos[rhoblock_start:rhoblock_end]
 
-Ts = Ts[Tsblock_start:Tsblock_end]
-
-print("Ts for this simulation batch: " + str(Ts))
+print("rhos for this simulation batch: " + str(rhos))
 
 for M in Ms:
     for T in Ts:
@@ -25,7 +28,7 @@ for M in Ms:
             print("M:   " + str(M))
             print("T:   " + str(T))
             print("rho: " + str(rho))
-            filename = "results/argon-M-%d-T-%.2f-rho-%.2f-simjob-%d.npz" % (M,T,rho,simJob)
+            filename = "results/argon-longeq-M-%d-T-%.2f-rho-%.2f-simjob-%d.npz" % (M,T,rho,simJob)
             sim.initialise_arrays(T, rho, M)
             sim.do_simulation()
             sim.save_results(filename)
--- molecular-dynamics/simulation/transcribe_results.py
+++ molecular-dynamics/simulation/transcribe_results.py
@@ -1,10 +1,10 @@
 import numpy as np
 
 Ms = [6]
-Ts = np.linspace(0.0, 4.0, 17)
+Ts = np.linspace(0.25, 4.0, 16)
 rhos = np.concatenate((np.array([0.02, 0.05]), np.linspace(0.1, 2.0, 20)))
 
-simJobs = np.arange(1,6)
+simJobs = [1]
 
 resultFiles = []
 
@@ -12,11 +12,11 @@ for M in Ms:
     for T in Ts:
         for rho in rhos:
         	for simJob in simJobs:
-        		resultFiles.append("argon-M-%d-T-%.2f-rho-%.2f-simjob-%d.npz" % (M,T,rho,simJob))
+        		resultFiles.append("argon-M-%d-T-%.2f-rho-%.2f" % (M,T,rho))
 
 for rFile in resultFiles:
 	print("Processing file: " + rFile)
-	with np.load("results/" + rFile) as data:
+	with np.load("results-longrun/" + rFile + "-simjob-1.npz") as data:
 		newData = dict()
 		for key in data:
 			if not (key == "distance_histogram"):
@@ -27,4 +27,4 @@ for rFile in resultFiles:
 		#newData["temperature"] = 2*data["kinetic_energy"]/(3*(data["n"] - 1))
 		
 		#newData["pressure"] = 1 + 1/(3 * data["n"] * np.mean(newData["temperature"])) * np.mean(data["virial"])
-		np.savez("results-nohist/" + rFile, **newData)
+		np.savez("results-longrun-nohist/" + rFile + "-simjob-1.npz", **newData)
